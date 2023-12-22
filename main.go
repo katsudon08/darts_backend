5f3611f0a1e50631b0ad7831e30274c853221349
matsu <matsuaki171101@gmail.com>
2023-12-22 10:15:21 +0900

    基本的なサーバー立ち上げのコード作成

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	hello := []byte("hello world")
+	_, err := w.Write(hello)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
-	fmt.Println("HELLO WORLD!")
+	http.HandleFunc("/", helloHandler)
+	fmt.Println("Server Start Up....")
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
