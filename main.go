84162e1640ce94b0d73f2f9b7fc7c09700e15cf2
matsu <matsuaki171101@gmail.com>
2023-12-27 00:40:34 +0900

    複数クライアントとの接続、メッセージ通信が完成

--- main.go
+++ main.go
@@ -7,47 +7,62 @@ import (
 	"golang.org/x/net/websocket"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan string)
+
+func handleHello(w http.ResponseWriter, r *http.Request) {
 	hello := []byte("hello world")
 	_, err := w.Write(hello)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
 	}
 }
 
-func webSocketHandler(w http.ResponseWriter, r *http.Request) {
-	websocket.Handler(func(ws *websocket.Conn) {
-		// 関数ブロックを抜ける前に必ず実行する
-		defer ws.Close()
+func handleConnection(ws *websocket.Conn) {
+	defer ws.Close()
 
-		err := websocket.Message.Send(ws, "Server: Hello World!")
+	err := websocket.Message.Send(ws, "hello.")
+	if err != nil {
+		log.Print(err)
+	}
+
+	clients[ws] = true
+
+	// メッセージの受信
+	for {
+		msg := ""
+
+		err := websocket.Message.Receive(ws, &msg)
 		if err != nil {
-			log.Fatal(err)
+			log.Print(err)
 		}
 
-		for {
-			msg := ""
-			err := websocket.Message.Receive(ws, &msg)
-			if err != nil {
-				if err.Error() == "EOF" {
-					log.Println(fmt.Errorf("read %s", err))
-					break
-				}
-				log.Fatal(err)
-			}
+		// 受取ったメッセージをbroadcastチャネルに送る(awaitのような感じ)
+		broadcast <- msg
+	}
+}
 
-			err = websocket.Message.Send(ws, fmt.Sprintf("Server: \"%s\" received", msg))
+func handleMessage() {
+	for {
+		// broadcastからメッセージを受取る
+		msg := <- broadcast
+
+		// 各クライアントへのメッセージの送信
+		for client := range clients {
+			err := websocket.Message.Send(client, fmt.Sprintf("received message : %s", msg))
 			if err != nil {
-				log.Fatal(err)
+				log.Print(err)
 			}
 		}
-	}).ServeHTTP(w, r)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", helloHandler)
-	http.HandleFunc("/socket", webSocketHandler)
+	http.HandleFunc("/", handleHello)
+	http.Handle("/ws", websocket.Handler(handleConnection))
+	go handleMessage()
+
 	fmt.Println("serving at http://localhost:8080....")
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
