cf474c19370ef932c8076168b4fa7628467115f6
matsu <matsuaki171101@gmail.com>
2024-01-04 00:08:17 +0900

    clientを階層ごとに分けることで、websocketの通信に成功

--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
 	"golang.org/x/net/websocket"
 )
 
-var clients = make(map[*websocket.Conn]bool)
-var broadcast = make(chan string)
+type Handler func (*websocket.Conn, string)
+
+var clients = make(map[string]map[*websocket.Conn]bool)
+var broadcast = make(chan Data)
 
 func handleHello(w http.ResponseWriter, r *http.Request) {
 	hello := []byte("hello world")
@@ -22,7 +25,11 @@ func handleHello(w http.ResponseWriter, r *http.Request) {
 func handleConnection(ws *websocket.Conn) {
 	defer ws.Close()
 
-	clients[ws] = true
+	key := ws.Request().URL.String()[1:]
+	clients[key] = make(map[*websocket.Conn]bool)
+	clients[key][ws] = true
+
+	fmt.Println(clients)
 
 	// メッセージの受信
 	for {
@@ -30,24 +37,31 @@ func handleConnection(ws *websocket.Conn) {
 
 		err := websocket.Message.Receive(ws, &msg)
 		if err != nil {
+			if err.Error() == "EOF" {
+				log.Fatal(fmt.Errorf("read %s", err))
+			}
 			log.Print(err)
 		}
 
 		fmt.Println(msg)
 
+		data := Data{key, msg}
+
+		fmt.Println(data)
+
 		// 受取ったメッセージをbroadcastチャネルに送る(awaitのような感じ)
-		broadcast <- msg
+		broadcast <- data
 	}
 }
 
 func handleMessage() {
 	for {
 		// broadcastからメッセージを受取る
-		msg := <- broadcast
+		data := <- broadcast
 
 		// 各クライアントへのメッセージの送信
-		for client := range clients {
-			err := websocket.Message.Send(client, msg)
+		for client := range clients[data.Key] {
+			err := websocket.Message.Send(client, data.Msg)
 			if err != nil {
 				log.Print(err)
 			}
--- types.go
+++ types.go
@@ -1,5 +1,10 @@
 package main
 
+type Data struct {
+	Key string
+	Msg string
+}
+
 const (
 	GROUP = "group"
 	TURN = "turn"
